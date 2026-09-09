68aad7fa9d68de44fd1d869762adb22a36ee5331
Mayank Kumar <mayankorai1200@gmail.comL>
2026-09-09 16:01:18 +0530

    feat: add DNS test utility and configure proxy bypass settings for internal services and mail server

---
+++ TestDNS.java
@@ -0,0 +1,1 @@
+import java.net.InetAddress; public class TestDNS { public static void main(String[] args) throws Exception { System.out.println(InetAddress.getByName("smtp.gmail.com")); } }
--- src/main/java/com/cris/customerportal/CustomerPortalApplication.java
+++ src/main/java/com/cris/customerportal/CustomerPortalApplication.java
@@ -8,8 +8,8 @@ public class CustomerPortalApplication {
   static {
     // Ensure local/internal hosts bypass any configured HTTP/SOCKS proxy
     // (fixes proxy errors when connecting to the Oracle DB / internal services).
-    System.setProperty("http.nonProxyHosts", "localhost|127.0.0.1|*.crisexacc.org|tndexaccvm-scan.crisexacc.org|db.internal.net");
-    System.setProperty("socksNonProxyHosts", "localhost|127.0.0.1|*.crisexacc.org|tndexaccvm-scan.crisexacc.org|db.internal.net");
+    System.setProperty("http.nonProxyHosts", "localhost|127.0.0.1|*.crisexacc.org|tndexaccvm-scan.crisexacc.org|db.internal.net|smtp.gmail.com");
+    System.setProperty("socksNonProxyHosts", "localhost|127.0.0.1|*.crisexacc.org|tndexaccvm-scan.crisexacc.org|db.internal.net|smtp.gmail.com");
   }
 
   public static void main(String[] args) { SpringApplication.run(CustomerPortalApplication.class, args); }
